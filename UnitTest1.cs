eadee7a38aa7b558ce5f0c972ae20160a7a98801
Afreed15 <Afreed.vs786@gmail.com>
2021-03-23 15:28:10 +0530

    my commit

---
+++ UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestExe
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSquare()
+        {
+            calLib.Calculation objs = new calLib.Calculation();
+            double num1 = 12.21;
+           
+            double result = num1*num1;
+            
+            Assert.AreEqual(result, objs.Square(num1));
+        }
+
+        [TestMethod]
+        public void TestCube()
+        {
+            calLib.Calculation objs = new calLib.Calculation();
+            double num = 12.21;
+            
+            double result = num *num*num;
+
+            Assert.AreEqual(result, objs.Cube(num));
+        }
+        [TestMethod]
+        public void TestSquareroot()
+        {
+            calLib.Calculation objs = new calLib.Calculation();
+            double num1 = 12.21;
+            
+            double result = 0.5*num1;
+
+            Assert.AreEqual(result, objs.SquareRoot(num1));
+        }
+    }
+}
---
+++ calLib/Calculation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace calLib
+{
+    internal class Calculation
+    {
+        internal double Square(double num1)
+        {
+            return num1 * num1;
+        }
+
+        internal double Cube(double num)
+        {
+            return num * num * num;
+        }
+
+        internal double SquareRoot(double num1)
+        {
+            return 0.5 * num1;
+        }
+    }
+}
